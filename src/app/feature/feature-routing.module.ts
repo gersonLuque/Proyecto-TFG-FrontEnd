2f08982973ea1662e372a3d36f0da79a38782408
Gerson Luque <gerson126@hotmail.com>
2025-02-08 12:52:20 +0100

    feat: agregado el componenet user-create

--- src/app/feature/feature-routing.module.ts
+++ src/app/feature/feature-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '', redirectTo: 'auth/login', pathMatch: 'full'
+  }, // Redirige a /auth/login por defecto
+  {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then(f => f.AuthModule)
   }
