c9a731dfd978e8c091f08be2fc59f0ad2f5b0744
Gerson Luque <gerson126@hotmail.com>
2025-02-08 14:24:25 +0100

    feat: agregado el routing para el user-create-component

--- src/app/feature/dashboard/user-management/user-management-routing.module.ts
+++ src/app/feature/dashboard/user-management/user-management-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UserManagementComponent} from './user-management.component';
+import {CreateUserComponent} from './create-user/create-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    component : UserManagementComponent
+  },
+  {
+    path : 'create-user',
+    component : CreateUserComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
