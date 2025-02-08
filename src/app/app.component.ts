1977aa8d78926421c1462cc585da016bb683de94
Gerson Luque <gerson126@hotmail.com>
2025-02-08 11:57:36 +0100

    feat: agregado el routing y componentes del feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Router, RouterOutlet} from '@angular/router';
+import {FeatureModule} from './feature/feature.module';
+import {HeaderComponent} from './shared/components/header/header.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'proyecto-tfg-front';
+  title = 'proyecto-front';
 }
---
+++ src/app/feature/feature.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-feature',
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './feature.component.html',
+  styleUrl: './feature.component.css'
+})
+export class FeatureComponent {
+
+}
