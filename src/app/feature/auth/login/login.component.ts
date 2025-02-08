144f784e66183de5b29c1480dd4b0cc1cf943119
Gerson Luque <gerson126@hotmail.com>
2025-02-08 15:58:28 +0100

    feat: agregada validacion al html en login

--- src/app/feature/auth/login/login.component.ts
+++ src/app/feature/auth/login/login.component.ts
@@ -1,17 +1,30 @@
 import { Component } from '@angular/core';
 import {Router} from '@angular/router';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   imports: [
+    ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
+
 export class LoginComponent {
-  constructor(private router:Router) { }
+
+  public loginForm: FormGroup;
+  constructor(private router:Router, private fb:FormBuilder) {
+    this.loginForm = this.fb.group({
+      username : ['',Validators.required],
+      password : ['',Validators.required]
+    })
+  }
 
   public logIn(){
-    console.log('el login funciona')
+    if(this.loginForm.valid){
+      console.log('el formulario es valido')
+    }
   }
 }
