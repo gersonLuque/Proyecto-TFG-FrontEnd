b68f54c737fa10e03734b6331980bcb71b6b19bb
Gerson Luque <gerson126@hotmail.com>
2025-02-16 17:23:01 +0100

    feat: agregada la peticion para rellanar los cursos por checkbox en el componente create-user

--- src/app/feature/dashboard/user-management/create-user/create-user.component.ts
+++ src/app/feature/dashboard/user-management/create-user/create-user.component.ts
@@ -1,20 +1,60 @@
-import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {Course} from '../../../../core/dto/courseDto';
+import {CourseService} from '../../../../core/services/course.service';
+import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
+import {Observable, of} from 'rxjs';
 
 @Component({
   selector: 'app-create-user',
   imports: [
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AsyncPipe,
+    NgIf,
+    NgForOf
   ],
   templateUrl: './create-user.component.html',
   styleUrl: './create-user.component.css'
 })
-export class CreateUserComponent {
+export class CreateUserComponent implements OnInit {
+
+  courses$: Observable<Course[]>;
 
   public createUserForm: FormGroup;
 
-  constructor(private fb:FormBuilder) {
+  constructor(private fb: FormBuilder, protected courseService: CourseService) {
+    this.createUserForm = this.fb.group({
+      username: [''],
+      completeName: [''],
+      password: [''],
+      role: [''],
+      courses: this.fb.array([])
+    })
+  }
+  ngOnInit(): void {
+    this.courses$ = this.courseService.getCourses();
+  }
+  toggleCourse(course: Course) {
+    const ar = this.createUserForm.get('courses') as FormArray;
+    const existingIndex = ar.controls.findIndex(control =>
+      control.get('courseId').value === course.courseId // Asegúrate que coincida con course.courseId
+    );
+
+    if (existingIndex !== -1) {
+      ar.removeAt(existingIndex);
+    } else {
+      const newCourse = this.fb.group({
+        courseId: [course.courseId],
+        name: [course.name]
+      });
+      ar.push(newCourse);
+    }
+    // console.log(this.createUserForm.value);
+  }
+
+  submit() {
+    this.courseService.createUser()
   }
 
 }
