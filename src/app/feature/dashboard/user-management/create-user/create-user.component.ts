38899f4db51f2053029c34a807908a2a3a164044
Gerson Luque <gerson126@hotmail.com>
2025-02-17 17:24:26 +0100

    feat: agregado el formulario y la peticion post al servidor para crear el usuario

--- src/app/feature/dashboard/user-management/create-user/create-user.component.ts
+++ src/app/feature/dashboard/user-management/create-user/create-user.component.ts
@@ -1,12 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {Course} from '../../../../core/dto/courseDto';
-import {CourseService} from '../../../../core/services/course.service';
-import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
-import {Observable, of} from 'rxjs';
+import {Course} from '@core/dto/courseDto';
+import {CourseService} from '@core/services/course.service';
+import {AsyncPipe} from '@angular/common';
+import {firstValueFrom, Observable} from 'rxjs';
 import {Toast, ToastModule} from 'primeng/toast';
-import {Button} from 'primeng/button';
-import {MessageService} from 'primeng/api';
+import {createUserDto} from '@core/dto/createUserDto';
+import {ToastService} from '@core/services/toast.service';
+import {UserService} from '@core/services/user.service';
 
 
 @Component({
@@ -16,8 +17,7 @@ import {MessageService} from 'primeng/api';
     ReactiveFormsModule,
     AsyncPipe,
     Toast,
-    ToastModule,
-    Button,
+    ToastModule
   ],
   templateUrl: './create-user.component.html',
   styleUrl: './create-user.component.css'
@@ -28,18 +28,24 @@ export class CreateUserComponent implements OnInit {
 
   public createUserForm: FormGroup;
 
-  constructor(private fb: FormBuilder, protected courseService: CourseService,private messageService: MessageService) {
+  constructor(private fb: FormBuilder,
+              protected courseService: CourseService,
+              private userService: UserService,
+              private toastService: ToastService) {
+
     this.createUserForm = this.fb.group({
       username: [''],
       completeName: [''],
       password: [''],
-      role: [''],
+      rol: [''],
       courses: this.fb.array([])
     })
   }
+
   ngOnInit(): void {
     this.courses$ = this.courseService.getCourses();
   }
+
   toggleCourse(course: Course) {
     const ar = this.createUserForm.get('courses') as FormArray;
     const existingIndex = ar.controls.findIndex(control =>
@@ -58,10 +64,15 @@ export class CreateUserComponent implements OnInit {
     // console.log(this.createUserForm.value);
   }
 
-  submit() {
-  }
-
-  show() {
-    this.messageService.add({severity:'info', summary: 'Exito', detail: 'Usuario creado correctamente'});
+  async submit() {
+    const user: createUserDto = this.createUserForm.value;
+    await firstValueFrom(this.userService.createUser(user))
+    .then(() => {
+      this.toastService.showSuccess(`Usuario ${user.username} creado con éxito`);
+    })
+    .catch(error => {
+      const errorMessage = (typeof error?.error === 'string') ? error.error : '';
+      this.toastService.showError(`Error al crear el usuario ${user.username} \n ${errorMessage}`);
+    });
   }
 }
