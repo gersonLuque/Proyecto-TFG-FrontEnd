d0c1c44e328480dcaefb79b1bdb555c3f146449d
Gerson Luque <gerson126@hotmail.com>
2025-03-12 11:41:58 +0100

    feat: componetizado la lista de ficheros

--- src/app/feature/dashboard/task-management/create-solution/create-solution.component.ts
+++ src/app/feature/dashboard/task-management/create-solution/create-solution.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, input, OnInit} from '@angular/core';
+import {TaskDto} from '@core/dto/taskDto';
+import {CourseService} from '@core/services/course.service';
+import {TaskService} from '@core/services/task.service';
+import {Observable} from 'rxjs';
+import {AsyncPipe} from '@angular/common';
+import {FileListComponent} from '../../../../shared/components/file-list/file-list.component';
 
 @Component({
   selector: 'app-create-solution',
-  imports: [],
+  imports: [
+    AsyncPipe,
+    FileListComponent
+  ],
   templateUrl: './create-solution.component.html',
   styleUrl: './create-solution.component.css'
 })
-export default class CreateSolutionComponent {
-  
+export default class CreateSolutionComponent  implements OnInit{
+
+  taskId = input.required<number>()
+  id = input.required<number>() // courseId
+  task$:Observable<TaskDto>;
+
+  constructor(private taskService:TaskService) {
+
+  }
+  ngOnInit() {
+    this.task$ = this.taskService.getTaskById(this.taskId());
+  }
+
 }
