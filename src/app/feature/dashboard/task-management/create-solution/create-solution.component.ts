244cad7c721ac51e815633d3413697d6f07b0873
Gerson Luque <gerson126@hotmail.com>
2025-03-12 10:41:18 +0100

    feat: creado el routing para la creacion de la solucion

---
+++ src/app/feature/dashboard/task-management/create-solution/create-solution.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-solution',
+  imports: [],
+  templateUrl: './create-solution.component.html',
+  styleUrl: './create-solution.component.css'
+})
+export default class CreateSolutionComponent {
+  
+}
