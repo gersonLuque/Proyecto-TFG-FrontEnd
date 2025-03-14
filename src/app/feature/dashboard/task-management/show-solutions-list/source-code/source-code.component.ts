782f820a1314ffd90bafb143c6674f44f335e55f
Gerson Luque <gerson126@hotmail.com>
2025-03-14 12:30:26 +0100

    implementado el componente de ngprime tabs

--- src/app/feature/dashboard/task-management/show-solutions-list/source-code/source-code.component.ts
+++ src/app/feature/dashboard/task-management/show-solutions-list/source-code/source-code.component.ts
@@ -1,12 +1,30 @@
-import {Component, input} from '@angular/core';
+import {Component, input, OnInit} from '@angular/core';
+import {Tab, TabList, TabPanel, TabPanels, Tabs} from 'primeng/tabs';
+import {SolutionService} from '@core/services/solution.service';
+import {Observable} from 'rxjs';
+import {SolutionDto} from '@core/dto/solutionDto';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-source-code',
-  imports: [],
+  imports: [
+    Tabs,
+    TabList,
+    Tab,
+    TabPanels,
+    TabPanel
+  ],
   templateUrl: './source-code.component.html',
   styleUrl: './source-code.component.css'
 })
-export default class SourceCodeComponent {
-  prefix = input.required<string>();
-  fileId = input.required<number>();
+export default class SourceCodeComponent implements OnInit {
+  solutionId = input.required<number>();
+  solution$:Observable<SolutionDto>
+
+  constructor(private solutionService: SolutionService) {
+  }
+  ngOnInit() {
+    this.solution$ = this.solutionService.getSolutionById(this.solutionId());
+  }
+
 }
