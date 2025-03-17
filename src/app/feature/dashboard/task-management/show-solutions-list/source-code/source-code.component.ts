19a692d4a6c63707a8614fc48cb6f7f65b427e8c
Gerson Luque <gerson126@hotmail.com>
2025-03-17 13:57:02 +0100

    feat: implementado la pagina para ver el codigo fuente de las soluciones

--- src/app/feature/dashboard/task-management/show-solutions-list/source-code/source-code.component.ts
+++ src/app/feature/dashboard/task-management/show-solutions-list/source-code/source-code.component.ts
@@ -5,6 +5,7 @@ import {Observable} from 'rxjs';
 import {SolutionDto} from '@core/dto/solutionDto';
 import {AsyncPipe} from '@angular/common';
 import {HighlightJsDirective} from 'ngx-highlight-js';
+import {ProgressSpinner} from 'primeng/progressspinner';
 
 @Component({
   selector: 'app-source-code',
@@ -14,7 +15,9 @@ import {HighlightJsDirective} from 'ngx-highlight-js';
     Tab,
     TabPanels,
     TabPanel,
-    HighlightJsDirective
+    HighlightJsDirective,
+    AsyncPipe,
+    ProgressSpinner
   ],
   templateUrl: './source-code.component.html',
   styleUrl: './source-code.component.css'
@@ -22,11 +25,22 @@ import {HighlightJsDirective} from 'ngx-highlight-js';
 export default class SourceCodeComponent implements OnInit {
   solutionId = input.required<number>();
   solution$:Observable<SolutionDto>
+  code:string = "import"
 
-  constructor(private solutionService: SolutionService) {
-  }
+  constructor(private solutionService: SolutionService) {}
+
   ngOnInit() {
-    this.solution$ = this.solutionService.getSolutionById(this.solutionId());
+    this.solution$ = this.solutionService.getSolutionWithContent(this.solutionId())
   }
 
+  formatCode(code: string): string {
+    return code
+    .split('\n')
+    .map((line, index) => {
+      const lineNumber = (index + 1).toString();
+      const spaces = " ".repeat(3 - lineNumber.length); // Ajusta los espacios según la longitud del número
+      return `${lineNumber}.${spaces}${line}`;
+    })
+    .join('\n');
+  }
 }
